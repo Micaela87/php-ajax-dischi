9de3963cd619a9d0de1bbf848d7a287caa096493
Micaela87 <84184729+Micaela87@users.noreply.github.com>
2021-12-22 17:16:50 +0100

    project completed

---
+++ src/App.vue
@@ -0,0 +1,41 @@
+<template>
+  <div id="app">
+    <Header :genres="genresList" @sendOption="getOption"/>
+    <Main :genre="selectedGenre" @sendGenreList="getGenreList"/>
+  </div>
+</template>
+
+<script>
+import Main from './components/Main/Main.vue';
+import Header from './components/Header/Header.vue';
+
+export default {
+  name: 'App',
+  components: {
+    Header,
+    Main
+  },
+  data() {
+    return {
+      genresList: [],
+      selectedGenre: '',
+    }
+  },
+  methods: {
+    getOption(value) {
+      this.selectedGenre = value;
+    },
+    getGenreList(arr) {
+      this.genresList = arr;
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+</style>
---
+++ src/components/Main/DiscCard.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="card-container">
+    <img :src="details.poster" :alt="details.title">
+    <h3>{{ details.title }}</h3>
+    <div class="details">
+      <div class="artist">{{ details.author }}</div>
+      <div class="year">{{ details.year }}</div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'DiscCard',
+  props: {
+    details: Object
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+  .card-container {
+    width: 19%;
+    height: fit-content;
+    padding: 1.5rem;
+    background-color: #2e3a46;
+    text-align: center;
+    margin: 1rem 0.5rem;
+    min-height: 40rem;
+  }
+
+  img {
+    max-width: 100%;
+  }
+
+  h3 {
+    text-transform: uppercase;
+    color: white;
+    font-size: 1.8rem;
+    margin: 2rem 0;
+  }
+
+  .details {
+    color: #656f76;
+  }
+
+  .artist {
+    font-size: 1.6rem;
+  }
+
+  .yera {
+    font-size: 1.4rem;
+  }
+</style>
